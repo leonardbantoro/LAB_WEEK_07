390caf562c16d9e275418adb09858bd67c637e5f
leonardbantoro <leonardbantoro08@gmail.com>
2025-10-06 15:46:27 +0700

    Commit No. 1 – Create MapsActivity and location permission request

---
+++ app/src/main/java/com/example/lab_week_07/MapsActivity.kt
@@ -0,0 +1,87 @@
+package com.example.lab_week_07
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.example.lab_week_07.databinding.ActivityMapsBinding
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+
+//This is the variable through which we will launch the permission request and track user responses
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    getLastLocation()
+                } else {
+                    showPermissionRationale {
+                        requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                    }
+                }
+            }
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun hasLocationPermission() =
+        ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        when {
+            hasLocationPermission() -> getLastLocation()
+            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
+                showPermissionRationale {
+                    requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                }
+            }
+            else -> requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun getLastLocation() {
+        Log.d("MapsActivity", "getLastLocation() called.")
+    }
+
+    private fun showPermissionRationale(positiveAction: () -> Unit) {
+        AlertDialog.Builder(this)
+            .setTitle("Location permission")
+            .setMessage("This app will not work without knowing your current location")
+            .setPositiveButton(android.R.string.ok) { _, _ -> positiveAction() }
+            .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
+            .create().show()
+    }
+}
